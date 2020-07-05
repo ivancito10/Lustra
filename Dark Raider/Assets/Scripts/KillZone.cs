c355d49448c15c95455eb5c07334c6899f407536
ivancito10 <50561919+ivancito10@users.noreply.github.com>
2020-07-05 15:05:05 -0400

    Compartir Lustra

---
+++ Dark Raider/Assets/Scripts/KillZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillZone : MonoBehaviour {
+
+	public float damage = 100.0f;
+	public bool ac = true;
+
+	void OnTriggerStay2D(Collider2D otherCollider){
+
+		if (!otherCollider.CompareTag ("Player"))
+			return;
+
+
+		if (PlayerController.player != null) {
+			PlayerController.Health -= damage * Time.deltaTime;
+
+		}
+
+	}
+
+
+}
